26bfa7cac075d88aec21ed08f2646d1da47826fe
Yori Verbist <yori.verbist12@gmail.com>
2024-08-12 18:26:05 +0200

    Updated lazyvim config

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,13 @@
-return {
-    -- Copilot config
-    "zbirenbaum/copilot.lua",
-    opts = {
-        -- These are disabled in the default configuration.
-        suggestion = { enabled = true },
-        panel = { enabled = true },
-        filetypes = {
-            tex = false,
-            markdown = true,
-        },
-    },
-}
+-- return {
+--     -- Copilot config
+--     "zbirenbaum/copilot.lua",
+--     opts = {
+--         -- These are disabled in the default configuration.
+--         suggestion = { enabled = true },
+--         panel = { enabled = true },
+--         filetypes = {
+--             tex = false,
+--             markdown = true,
+--         },
+--     },
+-- }
