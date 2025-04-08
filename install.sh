3f2630ed50c35287f2ed12791c06131241c7f20c
Yori Verbist <yori.verbist12@gmail.com>
2025-04-08 17:13:11 +0200

    Removed some packages since it's better to build them

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 echo 'Pull the repo to check for updates'
-git pull origin master;
+git pull origin master
 
-cd $HOME;
+cd $HOME
 
 # Install needed packages
 sudo apt-get update
 echo 'Start installing all the packages'
-sudo apt-get install -y i3 feh compton kitty neovim tmux zsh curl zathura redshift
+sudo apt-get install -y i3 feh compton kitty zsh curl zathura redshift
 
 echo 'Installing oh-my-zsh'
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
